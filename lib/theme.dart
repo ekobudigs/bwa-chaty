981fcaef654d9bfcd515dd26006150f7967dacb0
ekobudigs <86825878+ekobudigs@users.noreply.github.com>
2021-08-03 11:56:48 +0700

    create theme

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = Color(0xff1F8DF5);
+Color whiteColor = Color(0xffFFFFFF);
+Color lightBlueColor = Color(0xff9ED0FF);
+Color blakColor = Color(0xff2C3A59);
+Color greyColor = Color(0xffE1E8EB);
+Color greenColor = Color(0xff29CB9E);
+
+TextStyle titleTextStyle = TextStyle(
+  color: blakColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 16,
+);
+
+TextStyle subtitle = TextStyle(
+  color: greenColor,
+  fontWeight: FontWeight.w300,
+);
