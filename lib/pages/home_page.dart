efe6b47fbceabf68691d1cecbbdde55c89a0bdbd
ekobudigs <86825878+ekobudigs@users.noreply.github.com>
2021-08-05 11:05:57 +0700

    desain awal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,41 @@
+import 'package:bwa_chaty/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('assets/images/friends1.png'),
+      backgroundColor: blueColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                'assets/images/friends3.png',
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Eko Budi',
+                style: TextStyle(fontSize: 20, color: whiteColor),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text(
+                'Liburan',
+                style: TextStyle(color: lightBlueColor, fontSize: 16),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
